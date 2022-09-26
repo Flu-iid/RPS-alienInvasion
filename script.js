342854b951143ae58125e7c7d67b5ead928005ea
Flu-iid <arshamm6@gmail.com>
2022-09-26 17:07:15 +0330

    started styling

--- script.js
+++ script.js
@@ -16,33 +16,37 @@ function play(championMove) {
       }
       break;
     case moves[1]:
-        switch (computerMove) {
-            case 0:
-              return +1;
-            case 1:
-              return 0;
-            case 2:
-              return -1;
-          }
+      switch (computerMove) {
+        case 0:
+          return +1;
+        case 1:
+          return 0;
+        case 2:
+          return -1;
+      }
       break;
     case moves[2]:
-        switch (computerMove) {
-            case 0:
-              return -1;
-            case 1:
-              return +1;
-            case 2:
-              return 0;
-          }
+      switch (computerMove) {
+        case 0:
+          return -1;
+        case 1:
+          return +1;
+        case 2:
+          return 0;
+      }
       break;
     default:
       break;
   }
 }
 
- for (let i = 5; i>0; i--){
-    console.log(play(window.prompt("enter 0 1 2")))
+function play2(championMove) {
+  const computerMove = Math.floor(Math.random() * 3);
+  const moves = [[0, -1, +1], [+1, 0, -1], [-1, +1, 0]];
+  const moveNames = ["rock", "paper", "scissor"];
+  console.log(`Alien did : ${moveNames[computerMove]}`);
 }
 
-
-
+for (let i = 5; i > 0; i--) {
+  play2(1)
+}
