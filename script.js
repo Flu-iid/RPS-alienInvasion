14e2567f3e2768959098a2cf9da0711a511e4842
Flu-iid <arshamm6@gmail.com>
2022-10-03 22:12:00 +0330

    design functions with js DOM manipulation is done Next designing play elements and final touch

--- script.js
+++ script.js
@@ -43,25 +43,40 @@ function result(point) {
       break;
   }
 }
-result(movePoint(1, alienPlay()));
 
 // Design
-timeFlag = false;
-function fadeElement() {
-  const elementShow = document.querySelector(".show");
-  const elementHide = document.querySelector(".hide");
-  if (elementShow) {
-    elementShow.className = "hide"
-    elementHide.className = "show"
-  } else {
-    elementHide.className = "show"
-  }
+let counter = 0;
+function createElement(content) {
+  const parent = document.querySelector(`.preview`);
+  const child = document.createElement("p");
+  parent.appendChild(child);
+  child.classList.add("message");
+  child.textContent = content;
+  counter++;
+  return counter;
 }
 
-function fadeTimer(count ,timeout) {
-  for (let i=count; i>0; i--) {
-    let myTime = setInterval(fadeElement(myTime), timeout)
-  }
+function timeNamer(i, content, time) {
+  window[`time` + i] = setTimeout(() => {
+    createElement(content);
+  }, time);
+}
+//function Run
+const contentList = [
+  "Hurry",
+  "7-leg-Aliens have invaded, and they left us with a challenge!",
+  "A game they call it ... which is apparently Rock-Paper-Scissors and they play it till point 7.",
+  "Our planet fates is in your hands champion!",
+  "Do what you must...",
+];
+
+function messagePreview(list, timeInterval) {
+  let totalTime = 0;
+  list.forEach((item, index, array) => {
+    timeNamer(counter, item, totalTime);
+    totalTime += timeInterval;
+  });
 }
 
-fadeTimer(5, 2000)
+messagePreview(contentList, 1000);
+result(movePoint(1, alienPlay()));
